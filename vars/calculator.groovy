ee8d7090e13c4a4b6ce3f5ab4380de044df9b96b
afzalhaider1 <131112928+afzalhaider1@users.noreply.github.com>
2023-08-25 09:28:18 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -1,10 +1,10 @@
 def add(int a, int b) {
     def sum = a + b
-    echo "Sum of ${a} and ${b} is ${sum}"
+    sh 'echo "Sum of ${a} and ${b} is ${sum}"'
     return sum
 }
 def sub(int a, int b) {
     def sub = a - b
-    echo "Subtraction of ${a} and ${b} is ${sub}"
+    sh 'echo "Subtraction of ${a} and ${b} is ${sub}"'
     return sub
 }
