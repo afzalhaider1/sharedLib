4fe7735a1e0f0f563bf6450b8a7376b542e95568
afzalhaider1 <131112928+afzalhaider1@users.noreply.github.com>
2024-11-10 09:45:12 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String project, String tag){
   echo "Pushing code to Docker Hub"
   withCredentials([usernamePassword(credentialsId: 'dockerHub', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
   sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  sh "docker push ${dockerhubuser}/${project}:${tag}"
+  sh "docker push ${dockerHubUser}/${project}:${tag}"
 }
